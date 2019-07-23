854d83ee1c880e951fd4afb468b25e65e488d860
Patricia Falandysz <pfalandysz@yahoo.com>
2019-07-23 23:18:36 +0200

    done rick and morty

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
+import App from './components/app';
 
 // const Hello = ({ name }) => {
 //   return (
@@ -12,32 +13,32 @@ import '../assets/stylesheets/application.scss';
 //   );
 // };
 
-class Hello extends Component {
-  constructor(props) {
-    super(props);
+// class Hello extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      clicked: false
-    };
-  }
+//     this.state = {
+//       clicked: false
+//     };
+//   }
 
-  handleClick = () => {
-    this.setState({
-      clicked: !this.state.clicked
-    });
-  }
+//   handleClick = () => {
+//     this.setState({
+//       clicked: !this.state.clicked
+//     });
+//   }
 
-  render() {
-    return (
-      <div className= {this.state.clicked ? "clicked" : ""} onClick={this.handleClick}>
-      Hello,
-      {this.props.name}
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className= {this.state.clicked ? "clicked" : ""} onClick={this.handleClick}>
+//       Hello,
+//       {this.props.name}
+//       </div>
+//     );
+//   }
+// }
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="Romeo" />, root);
+  ReactDOM.render(<App />, root);
 }
